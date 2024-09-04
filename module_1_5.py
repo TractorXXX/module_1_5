3bf09e73c10db3ef903672268bdd4ba58191cc00
Константин <tractorx@yandex.ru>
2024-09-04 15:35:28 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,10 @@
 immutable_var = 1, 'Urban', 134, True
 print(immutable_var)
+
 # immutable_var[0] = 5
 remark = 'Кортеж - это неизменяемый вид коллекций. Изменить его не получится'
 print(remark)
+
 mutable_list = [2, 23, 'List', False]
+mutable_list[1] = 745
 print(mutable_list)
